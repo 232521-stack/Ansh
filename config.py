980a9e65aa71191cf90e0296e603ef1f4c205bf7
232521-stack <232521@kit.ac.in>
2025-11-07 15:33:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8376204434:AAH7QrzzEQ6QBsjViynOEP6u2WM72gF2TCk")
+    API_ID = int(os.environ.get("API_ID", "20135931"))
+    API_HASH = os.environ.get("API_HASH", "60fb3f132eda81a31e0f77177ef95a75")
     AUTH_USERS = "1411895712"
+
 
